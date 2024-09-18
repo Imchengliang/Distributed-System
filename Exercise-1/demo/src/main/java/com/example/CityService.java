45a28d0a16eafd719697bb50faea8c8f3191bc6e
DevBilalAhmed <111248192+DevBilalAhmed@users.noreply.github.com>
2024-09-18 15:27:34 +0200

    creating methods for the server and manually testing them in main

--- Exercise-1/demo/src/main/java/com/example/CityService.java
+++ Exercise-1/demo/src/main/java/com/example/CityService.java
@@ -2,20 +2,24 @@ package com.example;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class CityService implements CountryInterface{
+public class CityService implements CityInterface {
 
     //maybe make cities final
     private List<City> cities;
 
     public CityService(String csvFilePath) throws IOException {
         // Use CSVReader to load the cities from the CSV file
+        // might move this to its own method for caching later
         CSVDatabase csvDatabase = new CSVDatabase();
         this.cities = csvDatabase.readCitiesFromCSV(csvFilePath);
     }
 
 
+    //test method.
     public void printFirstFiveCities() {
         System.out.println("First 5 cities from the CSV:");
         for (int i = 0; i < Math.min(5, cities.size()); i++) {
@@ -25,21 +29,84 @@ public class CityService implements CountryInterface{
 
     @Override
     public int getPopulationOfCountry(String countryName) throws RemoteException {
-        return 0;
+        int totalPopulation = 0;
+        for (City city : cities) {
+            if (city.getCountryNameEN().equalsIgnoreCase(countryName)){
+                totalPopulation += city.getPopulation();
+            }
+        }
+        //might remove the sout later
+        System.out.println(totalPopulation);
+        return totalPopulation;
     }
 
     @Override
     public int getNumberOfCities(String countryName, int min) throws RemoteException {
-        return 0;
+        int numberOfCities = 0;
+        for (City city : cities) {
+            if (city.countryNameEN.equalsIgnoreCase(countryName) && city.getPopulation() >= min){
+                numberOfCities += 1;
+            }
+        }
+        System.out.println(numberOfCities);
+        return numberOfCities;
     }
 
     @Override
     public int getNumberOfCountries(int cityCount, int minPopulation) throws RemoteException {
-        return 0;
+        Map<String, Integer> countryCityCount = new HashMap<>();
+        for (City city : cities){
+            if (city.getPopulation() >= minPopulation){
+                String countryName = city.getCountryNameEN();
+
+                //if countryCityCount already exists add 1 if not initialize it with the value 1
+                if (countryCityCount.containsKey(countryName)){
+                    countryCityCount.put(countryName, countryCityCount.get(countryName) +1);
+                }else {
+                    countryCityCount.put(countryName,1);
+                }
+            }
+        }
+
+        int numberOfCountries = 0;
+
+        //traverse hashmap to return values if they are above cityCount
+        for (int count : countryCityCount.values()){
+            if (count >= cityCount) {
+                numberOfCountries++;
+            }
+        }
+
+        System.out.println(numberOfCountries);
+        return numberOfCountries;
     }
 
     @Override
-    public int getNumberOfCountries2(int cityCount, int minPopulation, int maxPopulation) throws RemoteException {
-        return 0;
+    public int getNumberOfCountries(int cityCount, int minPopulation, int maxPopulation) throws RemoteException {
+        Map<String, Integer> countryCityCount = new HashMap<>();
+        for (City city : cities){
+            if (city.getPopulation() >= minPopulation && city.getPopulation() <= maxPopulation){
+                String countryName = city.getCountryNameEN();
+
+                //if countryCityCount already exists add 1 if not initialize it with the value 1
+                if (countryCityCount.containsKey(countryName)){
+                    countryCityCount.put(countryName, countryCityCount.get(countryName) +1);
+                }else {
+                    countryCityCount.put(countryName,1);
+                }
+            }
+        }
+
+        int numberOfCountries = 0;
+
+        //traverse hashmap to return values if they are above cityCount
+        for (int count : countryCityCount.values()){
+            if (count >= cityCount) {
+                numberOfCountries++;
+            }
+        }
+
+        System.out.println(numberOfCountries);
+        return numberOfCountries;
     }
 }
--- Exercise-1/demo/src/main/java/com/example/Main.java
+++ Exercise-1/demo/src/main/java/com/example/Main.java
@@ -8,7 +8,16 @@ public class Main {
             CityService cityService = new CityService("Exercise-1/demo/src/main/resources/dataset/exercise_1_dataset.csv");
 
             // Call the method to print the first 5 cities to make sure it runs
-            cityService.printFirstFiveCities();
+            //cityService.printFirstFiveCities();
+            //testing getPopulationOfCountry
+            cityService.getPopulationOfCountry("Norway");
+            cityService.getPopulationOfCountry("Sweden");
+            //testing getNumberOfCities
+            cityService.getNumberOfCities("Norway", 100000);
+            //testing cityCount and minValue
+            cityService.getNumberOfCountries(2, 5000000);
+            //testing cityCount with min and maxValue
+            cityService.getNumberOfCountries(30, 100000, 800000);
 
         } catch (IOException e) {
             e.printStackTrace();
